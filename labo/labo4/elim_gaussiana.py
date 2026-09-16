bab00f8b72f723c7dee5925333d1dbca6513901b
tomihq <hernandeztomas584@gmail.com>
2026-09-16 00:23:04 -0300

    add: relacionLUMatriz

--- labo/labo4/elim_gaussiana.py
+++ labo/labo4/elim_gaussiana.py
@@ -18,7 +18,6 @@ def calculaLU(A):
         
         if np.isclose(Ac[columna][columna], 0.0, rtol=1e-3):
             return None, None, 0
-        print(Ac[columna][columna])
         for fila in range(columna + 1, m):
             val = Ac[fila, columna] / Ac[columna, columna]
             Ac[fila, columna] = val 
@@ -67,6 +66,14 @@ def calculaLU(A):
     
 """
 
+def norma1(A):
+        return max(sum(abs(A[i][j]) for i in range(A.shape[0]))
+                   for j in range(A.shape[1]))
+
+def normainf(A):
+        return max(sum(abs(A[i][j]) for j in range(A.shape[1]))
+                  for i in range(A.shape[0]))
+
 def estimarError(sizes):
     errores = []
     operaciones = []
@@ -89,4 +96,43 @@ def estimarError(sizes):
     
     return operaciones, errores
 
-print(estimarError([5, 10, 20, 50, 100, 200]))
+"""
+    2. Estudiar la relación entre la cantidad de operaciones para encontrar la factorización LU y el tamaño de la matriz.
+
+    Propongo tomar una matriz sencilla que tenga LU y nos permita hacer operaciones triviales.
+        -1 si i>j
+        1 si i==j o j == n-1
+        0 cc
+
+
+    El algoritmo de la factorización LU aparenta ser determinístico en cantidad de pasos. Es decir, la decomposición es única, y por ende, la cantidad de operaciones para una misma matriz de dimensión n siempre será la misma.
+
+    (Notar que la norma infinito de U_n de casualidad es 2^{n-1} porque la ultima columna va creciendo a su valor, y como la norma infinito es el valor más grande lo agarra cuando termina).
+
+    
+"""
+def relacionLUMatriz(sizes):
+    cantidad_operaciones = []
+
+    for n in sizes:
+        matrix = np.zeros((n, n)) 
+
+        for i in range(n):
+            for j in range(n): 
+                if i == j or j == n-1:
+                    matrix[i][j] = 1
+                elif i>j:
+                    matrix[i][j] = -1
+                else:
+                    matrix[i][j] = 0
+        
+        L, U, cant_operaciones = calculaLU(matrix)
+        print(n)
+        norma_inf = normainf(U)
+        cantidad_operaciones.append([n, cant_operaciones, norma_inf, 2**(n-1)])
+
+    return cantidad_operaciones
+
+
+
+print(relacionLUMatriz([1, 2, 3, 4, 5, 10, 50, 100, 200, 250, 300]))
