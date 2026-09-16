a4cf6ddab1a1350eb7081c6a763f8df280df4bfb
tomihq <hernandeztomas584@gmail.com>
2026-09-15 22:54:41 -0300

    add: elim_gaussiana

--- labo/labo4/elim_gaussiana.py
+++ labo/labo4/elim_gaussiana.py
@@ -1,49 +1,31 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Eliminacion Gausianna
-"""
 import numpy as np
 
 def elim_gaussiana(A):
     cant_op = 0
-    m=A.shape[0]
-    n=A.shape[1]
+    m = A.shape[0]
+    n = A.shape[1]
     Ac = A.copy()
     
-    if m!=n:
+    if m != n:
         print('Matriz no cuadrada')
         return
-    
-    ## desde aqui -- CODIGO A COMPLETAR
 
-
-
-
-
-                
-    ## hasta aqui, calculando L, U y la cantidad de operaciones sobre 
-    ## la matriz Ac
+    for columna in range(m):
+        
+        if np.isclose(Ac[columna, columna], 0.0, rtol=1e-5):
+            print('Pivote cero o cercano a cero.')
+            return
             
-    
-    return L, U, cant_op
+        for fila in range(columna + 1, m):
+            val = Ac[fila, columna] / Ac[columna, columna]
+            Ac[fila, columna] = val 
+            
+            for k in range(columna+1, n):
+                Ac[fila, k] = Ac[fila, k] - val * Ac[columna, k]
+            
+            
+            cant_op += 1
+            
+    return Ac, cant_op
 
-
-def main():
-    n = 7
-    B = np.eye(n) - np.tril(np.ones((n,n)),-1) 
-    B[:n,n-1] = 1
-    print('Matriz B \n', B)
-    
-    L,U,cant_oper = elim_gaussiana(B)
-    
-    print('Matriz L \n', L)
-    print('Matriz U \n', U)
-    print('Cantidad de operaciones: ', cant_oper)
-    print('B=LU? ' , 'Si!' if np.allclose(np.linalg.norm(B - L@U, 1), 0) else 'No!')
-    print('Norma infinito de U: ', np.max(np.sum(np.abs(U), axis=1)) )
-
-if __name__ == "__main__":
-    main()
-    
     
